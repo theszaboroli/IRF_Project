e26aded1e4be569e531ef9d1bce283f1e4cc158e
Roland Szabo <theszaboroli@gmail.com>
2020-12-08 16:06:09 +0100

    login

--- auto/auto/Form1.cs
+++ auto/auto/Form1.cs
@@ -16,5 +16,39 @@ namespace auto
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Kérlek írd be a jelszót!");
+
+            }
+            else if (textBox1.Text == "IRF")
+            {
+                Form2 home = new Form2();
+
+                // Show the settings form
+
+                this.Hide();
+                home.Show();
+            }
+
+            else if (textBox1.Text == "EasterEGG")
+            {
+                this.BackColor = Color.Fuchsia;
+                
+            }
+
+            else
+            {
+                MessageBox.Show("Rossz jelszó!");
+            }
+        }
     }
 }
